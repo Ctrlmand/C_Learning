393e37381d714547b1feff73ab04603871d01f58
Ctrlmand <90202636+Ctrlmand@users.noreply.github.com>
2023-06-04 19:42:38 +0800

    233

--- Array/Superior_Prime/Prime_list(With in).c
+++ Array/Superior_Prime/Prime_list(With in).c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 int main (void)
 {
-    const int maxNumber = 10;
+    const int maxNumber = 15;
     int isPrime[maxNumber];
     int x;
     int i;
-
+//h
     {
         printf ("\t\t");
         int i;
-        for ( i=0; i<maxNumber; i++) {
+        for ( i=2; i<maxNumber; i++) {
             printf("%d\t", i);
         }
         printf ("\n");
     }
-
+//e
     for ( i=0; i<maxNumber; i++) {
         isPrime[i] = 1;
     }
@@ -24,16 +24,16 @@ int main (void)
                 isPrime[i*x] = 0;
             }
         }
-
+//h
         {
             printf ("x=%d\ti=%d\t", x, i);
             int i;
-            for ( i=0; i<maxNumber; i++) {
+            for ( i=2; i<maxNumber; i++) {
                 printf("%d\t",isPrime[i]);
             }
             printf ("\n");
         }
-
+//e
     }
     for ( i=2; i<maxNumber; i++) {
         if (isPrime[i]) {
