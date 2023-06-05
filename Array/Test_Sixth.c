831153743dcb466b79ff76b1af7851cf6d53730f
Ctrlmand <90202636+Ctrlmand@users.noreply.github.com>
2023-06-05 18:00:28 +0800

    233

---
+++ Array/Test_Sixth.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int serch( int key, int a[], int length);
+int main (void) 
+{
+    int a[10];
+    int n = 0;
+    do {
+        scanf ("%d %d", a[n], a[n+1]);
+        n += 2;
+
+    }
+    return 0;  
+} 
--- try.c
+++ try.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
-int main()
+int main (void) 
 {
-   for ( int i =2; i<100; i++) {
-    int isPrime = 1;
-        for ( int k = 2; k<i; k++) {
-            if ( i%k == 0) {
-                isPrime = 0;
-                break;
-            }
-        }
-    if (isPrime == 1) {
-        printf ("%d\n", i);
-    }
-   }
+
     return 0;
 }
