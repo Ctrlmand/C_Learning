0745f6dab8f6b332da734e3c68b8f20fb1c64b6c
Ctrlmand <90202636+Ctrlmand@users.noreply.github.com>
2023-06-08 17:57:27 +0800

    2023/6/8_Company

--- Array/Test_Sixth.c
+++ Array/Test_Sixth.c
@@ -2,12 +2,46 @@
 int serch( int key, int a[], int length);
 int main (void) 
 {
-    int a[10];
-    int n = 0;
+    //const int number = 15;
+    int store [15] = {0};
+    int exit = -1;
+    int cnt = 0;
+    int i = 0;
     do {
-        scanf ("%d %d", a[n], a[n+1]);
-        n += 2;
-
+        scanf ("%d %d", &store[i], &store[i+1]);
+        cnt +=2;
+        if (!store[i]) {
+            exit++;
+        }
+        i +=2;
+    } while (!exit);
+    //
+    {
+        for (int o=0; o<cnt; o++) {
+            printf ("%d\t", o);
+        }
+    printf ("\n");
+        for (int o=0; o<cnt; o++) {
+            printf ("%d\t", store[o]);
+        }
+        printf ("\n");
     }
+    //
+    
+    // for (i=0; i<cnt; i+2) {
+    //     if (b>0) {
+    //         if (store[i+1]>1) {
+    //             printf ("%d", store[i+1]);
+    //         }
+    //         if (store[i]>0) {
+    //             printf ("x");
+    //         }
+    //         if (store[i]>1) {
+    //            printf ("%d", store[i]); 
+    //         }
+    //     }
+    // }
+    
+
     return 0;  
 } 
