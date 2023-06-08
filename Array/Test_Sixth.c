eb52ef5a86a436326e8824a714bbf35e6818a72f
Ctrlmand <90202636+Ctrlmand@users.noreply.github.com>
2023-06-08 21:37:09 +0800

    2023/6/8_home

--- Array/Test_Sixth.c
+++ Array/Test_Sixth.c
@@ -2,20 +2,40 @@
 int serch( int key, int a[], int length);
 int main (void) 
 {
-    //const int number = 15;
-    int store [15] = {0};
-    int exit = -1;
-    int cnt = 0;
-    int i = 0;
+    //const int number = 20;
+    int store [20] = {0};
+    int exit=-1;
+    int cnt=0;
+    int i=0;
+    int tmp=0;
     do {
         scanf ("%d %d", &store[i], &store[i+1]);
         cnt +=2;
-        if (!store[i]) {
+        if (store[i]<1) {
             exit++;
         }
         i +=2;
-    } while (!exit);
-    //
+    } while (exit<1);
+    {
+        for (int o=0; o<cnt; o++) {
+            printf ("%d\t", o);
+        }
+    printf ("\n");
+        for (int o=0; o<cnt; o++) {
+            printf ("%d\t", store[o]);
+        }
+        printf ("\n\n");
+    }
+    for (i=0; i<cnt; i+=2) {
+        if (store[i] != -1) {
+            for (int t=i+2; t<cnt; t+=2) {
+                if (store[t] == store[i]) {
+                    store[i+1] += store[t+1];
+                    store[t] = store[t+1] = -1;
+                }
+            }
+        }
+    }
     {
         for (int o=0; o<cnt; o++) {
             printf ("%d\t", o);
@@ -25,23 +45,27 @@ int main (void)
             printf ("%d\t", store[o]);
         }
         printf ("\n");
+    }    
+    
+    for (i=0; i<cnt; i+=2) {
+        if (store[i+1]>0) {
+            if (store[i]==0) {
+                tmp=i;
+            } else {
+                if (store[i+1]>1) {
+                    printf ("%d", store[i+1]);
+                }
+                    printf ("x");
+                if (store[i]>1) {
+                printf ("%d", store[i]); 
+                }
+            printf ("+");
+            }
+        }
     }
-    //
-    
-    // for (i=0; i<cnt; i+2) {
-    //     if (b>0) {
-    //         if (store[i+1]>1) {
-    //             printf ("%d", store[i+1]);
-    //         }
-    //         if (store[i]>0) {
-    //             printf ("x");
-    //         }
-    //         if (store[i]>1) {
-    //            printf ("%d", store[i]); 
-    //         }
-    //     }
-    // }
-    
-
+    if (tmp != 0) {
+        printf ("%d", store[tmp+1]);
+    }
     return 0;  
 } 
+//4x6+6x5+12x3+12x2+12x+40
